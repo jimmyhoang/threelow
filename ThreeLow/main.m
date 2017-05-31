dad4cc9a53395b8a2d528da9f9fa00a083e95f76
Jimmy Hoang <hoangjimmy94@gmail.com>
2017-05-31 13:16:33 -0700

    initial creation

---
+++ ThreeLow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ThreeLow
+//
+//  Created by Jimmy Hoang on 2017-05-31.
+//  Copyright © 2017 Jimmy Hoang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
